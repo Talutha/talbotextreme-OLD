c07d81b76acf9b6016b3c7f8464b5bb6c4caf6b9
Talutha <talutha@gmail.com>
2016-07-15 20:10:53 -0500

    Initial setup and chat parsing

--- app.js
+++ app.js
@@ -1,5 +1,29 @@
-function hello() {
-  return "Hello world!";
-}
+var tmi = require('./lib/connect').client;
+var chat = require('./lib/chat');
 
-console.log(hello());
+tmi.connect();
+
+tmi.on('chat', function(channel, user, message, self) {
+  // Self returns BOOLEAN if chatter is the same as the bot
+  // Channel returns #channel-name
+  // Message returns message as a string
+  // User returns a user object:
+  /*
+  USER: {"badges":null,
+         "color":"#0000FF",
+         "display-name":"Talutha",
+         "emotes":null,
+         "id":"969bd3aa-388a-41ba-878b-21fb7f8db51d",
+         "mod":false,
+         "room-id":"28368365",
+         "subscriber":false,
+         "turbo":false,
+         "user-id":"15213012",
+         "user-type":null,
+         "emotes-raw":null,
+         "badges-raw":null,
+         "username":"talutha",
+         "message-type":"chat"}
+  */
+  chat.chatTest(tmi, channel, user, message, self);
+});
---
+++ test/tests.js
@@ -0,0 +1,63 @@
+/* jslint node: true */
+/* jslint esversion: 6 */
+/* jslint mocha: true */
+/* jslint expr: true */
+/* jslint -W024 */
+'use strict';
+
+const sinon = require('sinon');
+const chai = require('chai');
+const assert = chai.assert;
+const expect = chai.expect;
+
+const chat = require('../lib/chat');
+const tmi = require('../lib/connect').client;
+const tmiShort = 'fake';
+
+describe('Chat Test', function() {
+  it('should return success', function() {
+    let say = sinon.stub(tmi, 'say');
+    let expectedChannel = '#talbotextreme';
+    let expectedMessage = 'Test Successful Talutha!';
+    let user = { username: 'Talutha' };
+
+    chat.chatTest(tmi, expectedChannel, user, 'Nope');
+
+    say.restore();
+    sinon.assert.calledWith(say, expectedChannel, expectedMessage);
+  });
+});
+
+describe('Parsing Chat', function() {
+  let channel = '#testChannelPleaseIgnore';
+  let user = 'TestUser'
+  describe('parseChat', function() {
+    it('should do nothing when bot is the sender', function() {
+      let message = '!addpoints 900 testUser';
+      let parsed = chat.parseChat(tmiShort, channel, 'Talbotextreme', message, true);
+      expect(parsed).to.be.false;
+    });
+
+    it('should do nothing when message does not begin with "!"', function() {
+      let message = 'Test message, please ignore!';
+      let parsed = chat.parseChat(tmiShort, channel, user, message, false);
+      expect(parsed).to.be.false;
+    });
+
+    it('should send default commands to checkForCommand', function() {
+      let check = sinon.stub(chat, 'checkForCommand');
+      let message = '!points';
+      let parsed = chat.parseChat(tmiShort, channel, user, message, false);
+      let splitMessage = message.split(' ');
+
+      check.restore();
+      sinon.assert.calledWith(check, tmiShort, channel, user, splitMessage);
+    });
+  });
+
+  describe('checkForCommand', function() {
+    it('sends default commands to defaultCommands()');
+    it('sends all other commands to checkDBFor()');
+  });
+
+});
